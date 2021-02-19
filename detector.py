6c5a84729d307a1bd51f3f8087fad28ed55df104
JoannaWangBK <wangjoa@berkeley.edu>
2021-02-18 22:01:24 -0500

    fixed erros

--- detector.py
+++ detector.py
@@ -3,7 +3,7 @@ import cv2
 import paho.mqtt.client as mqtt
 import time
 
-MQTT_BROKER = "broker"
+MQTT_BROKER = "mosquitto"
 MQTT_TOPIC = "faces"
 
 # 1 should correspond to /dev/video1, the USB camera. The 0 is reserved for the TX2 onboard camera
@@ -16,18 +16,18 @@ mqttc.connect(MQTT_BROKER, port=1883)
 
 
 #create face detector
-facealg = cv.CascadeClassifier("/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml")
+facealg = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 #connect to camera
-cap = cv.VideoCapture(1)
+#cap = cv.VideoCapture(1)
 
 while(True):
 
     #read image in gray scale
     ret, frame = cap.read()
-    gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-    cv.imshow('frame', gray)
+    cv2.imshow('frame', gray)
 
     #detect a face
     faces = facealg.detectMultiScale(gray,1.3,5)
@@ -37,14 +37,14 @@ while(True):
         roi_gray = gray[y:y+h, x:x+w]
     
         #encode and public message
-        rc,png = cv.imencode('.png', roi_gray)
+        rc,png = cv2.imencode('.png', roi_gray)
         msg = pickle.dumps(png)
         mqtt_client.publish(LOCAL_MQTT_TOPIC, msg, qos=0, retain=False)
     
     #quit capturing
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
--- forwarder.py
+++ forwarder.py
@@ -13,7 +13,8 @@ def on_publish_remote(client,userdata,result):
 #create remote mqtt client
 remote_mqtt_client = mqtt.Client()
 remote_mqtt_client.on_publish = on_publish_remote
-remote_mqtt_client.connect(REMOTE_MQTT_HOST, REMOTE_MQTT_PORT)
+#Wait for AWS broker
+#remote_mqtt_client.connect(REMOTE_MQTT_HOST, REMOTE_MQTT_PORT)
 #remote_mqtt_client.publish(REMOTE_MQTT_TOPIC, "test remote connection")
 
 #local info
@@ -28,11 +29,12 @@ def on_connect_local(client, userdata, flags, rc):
 
 
 def on_message(client,userdata, msg):
-  try:
-    remote_mqtt_client.publish(REMOTE_MQTT_TOPIC, payload=msg.payload, qos=0, retain=False)
-  except:
-    print("Unexpected error:", sys.exc_info()[0])
-
+ # try:
+ #   remote_mqtt_client.publish(REMOTE_MQTT_TOPIC, payload=msg.payload, qos=0, retain=False)
+ # except:
+ #   print("Unexpected error:", sys.exc_info()[0])
+    print("Got msg")
+    
 local_mqttclient = mqtt.Client()
 local_mqttclient.on_connect = on_connect_local
 local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
