9e1169613f9df8c9b55df2093a79f76dc560fc86
Joanna Wang <wangjia@berkeley.edu>
2021-03-04 15:01:25 -0500

    updated processor

--- processor.py
+++ processor.py
@@ -1,33 +1,24 @@
+import numpy as np
+import cv2
 import paho.mqtt.client as mqtt
-import cv2 as cv
-import time
-import pickle
-from datetime import datetime
-
-#local info
-LOCAL_MQTT_HOST = "remote-broker"
-LOCAL_MQTT_PORT = 1883
-LOCAL_MQTT_TOPIC = "facedetector_topic_remote"
-PATH = "/mnt/mybucket/"
-
-#local callback function
-def on_connect_local(client, userdata, flags, rc):
-        print("image processor connected to remote broker with rc: " + str(rc))
-        client.subscribe(LOCAL_MQTT_TOPIC)
-
-
-def on_message(client,userdata, msg):
-  try:
-    print("\nimage to process received")
-
-
-  except:
-    print("Unexpected error:", sys.exc_info()[0])
-
-local_mqttclient = mqtt.Client()
-local_mqttclient.on_connect = on_connect_local
-local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
-local_mqttclient.on_message = on_message
-
-# go into a loop
-local_mqttclient.loop_forever()
+import math
+LOCAL_MQTT_HOST="mosquitto"
+LOCAL_MQTT_PORT=1883
+LOCAL_MQTT_TOPIC="faces"
+image_number = 0
+def on_connect(client, userdata, flags, rc):
+    print("connected to local broker with rc: " + str(rc))
+    client.subscribe(LOCAL_MQTT_TOPIC)
+def on_message(client, userdata, msg):
+    msg = np.frombuffer(msg.payload, dtype='uint8')
+    img = cv2.imdecode(msg, flags=1)
+    print(img.shape)
+    img_name = "face-" + str(image_number) + ".png"
+    print(f"{img_name}")
+    image_number += 1
+    cv2.imwrite('/mnt/mountpoint/'+img_name, img)
+mqttclient = mqtt.Client()
+mqttclient.on_connect = on_connect
+mqttclient.on_message = on_message
+mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
+mqttclient.loop_forever()
